1f3833a153314353e56e56d5430f4a3d1c810904
Mari26 <mari.qubro@gmail.com>
2021-09-11 02:25:15 +0400

    change controller

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -36,7 +36,26 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-
+       $request->validate([
+           'provider_id' => 'required',
+           'type_id' => 'required',
+           'name' => 'required',
+           'code' => 'required',
+           'price' => 'required',
+           'productiontime' => 'required',
+           'productionperiod' => 'required',
+       ]);
+       $newProduct= new Product([
+           'provider_id' => $request->get('provider_id'),
+           'type_id' => $request->get('type_id'),
+           'name' => $request->get('name'),
+           'code' => $request->get('code') ,
+           'price' =>$request->get('price') ,
+           'productiontime' => $request->get('productiontime') ,
+           'productionperiod' => $request->get('productionperiod') ,
+       ]);
+        $newProduct->save();
+        return response()->json($newProduct);
     }
 
     /**
@@ -47,7 +66,8 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product=Product::findOrfail($id);
+        return response()->json($product);
     }
 
     /**
@@ -70,7 +90,25 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product=Product::findOrfail($id);
+        $request->validate([
+            'provider_id' => 'required',
+            'type_id' => 'required',
+            'name' => 'required',
+            'code' => 'required',
+            'price' => 'required',
+            'productiontime' => 'required',
+            'productionperiod' => 'required',
+        ]);
+
+            $product->provider_id = $request->get('provider_id');
+            $product->type_id =$request->get('type_id');
+            $product->name =$request->get('name');
+            $product->code =$request->get('code') ;
+            $product-> price =$request->get('price') ;
+            $product->productiontime =$request->get('productiontime') ;
+            $product->productionperiod =$request->get('productionperiod');
+
     }
 
     /**
@@ -81,6 +119,8 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product=Product::findOrfail($id);
+        $product->delete();
+        return response()->json($product::all());
     }
 }
