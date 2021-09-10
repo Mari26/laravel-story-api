634e044d8fe6f6883314af6952e7c1b2f59092fa
Mari26 <mari.qubro@gmail.com>
2021-09-10 18:21:43 +0400

    laravel api project

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Provider;
+use App\Models\Type;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['provider_id','type_id','name','code','price','productiontime','productionperiod'];
+
+    public function providers()
+    {
+        return $this->belongsTo(Provider::class);
+    }
+//    public function types()
+//    {
+//        return $this->belongsTo(Types::class);
+//    }
+}
