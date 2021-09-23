0cbe369beddd930b6fc1d46f4d075a0d760b2e58
Mari26 <mari.qubro@gmail.com>
2021-09-23 11:21:14 +0400

    change controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,26 +15,17 @@ class ProductController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        /**
-         * Display a listing of the resource.
-         *
-         * @return \Illuminate\Http\Response
-         */
+     {
+      $products = Product::all();
 
-        $products = Product::all();
-        return ProductResource::collection($products);
-    }
+      return ProductResource::collection($products);
+     }
 
         /**
          * Show the form for creating a new resource.
          *
          * @return \Illuminate\Http\Response
          */
-        public function create()
-    {
-
-    }
 
         /**
          * Store a newly created resource in storage.
@@ -43,13 +34,13 @@ class ProductController extends Controller
          * @return \Illuminate\Http\Response
          */
         public function store(ProductRequest $request)
-    {
+        {
         $validated = $request->validated();
         $product = Product::create($validated);
 
         return ProductResource::collection($product);
 
-    }
+        }
 
         /**
          * Display the specified resource.
@@ -58,24 +49,13 @@ class ProductController extends Controller
          * @return \Illuminate\Http\Response
          */
         public function show($id)
-    {
+        {
         $product = Product::find($id);
         if (is_null($product)) {
             return $this->sendError('Product not found.');
         }
         return ProductResource::collection($product);
-    }
-
-        /**
-         * Show the form for editing the specified resource.
-         *
-         * @param  int  $id
-         * @return \Illuminate\Http\Response
-         */
-        public function edit($id)
-    {
-        //
-    }
+        }
 
         /**
          * Update the specified resource in storage.
@@ -85,21 +65,16 @@ class ProductController extends Controller
          * @return \Illuminate\Http\Response
          */
         public function update(ProductRequest $request, Product $product)
-    {
+        {
         $validated = $request->validated();
+
         if($validated->fails()){
             return $this->sendError('Validation Error.', $validated->errors());
         }
-        $product->provider_id = $validated['provider_id'];
-        $product->type_id = $validated['type_id'];
-        $product->name = $validated['name'];
-        $product->code = $validated['code'];
-        $product->price = $validated['price'];
-        $product->productiontime = $validated['productiontime'];
-        $product->productionperiod = $validated['productionperiod'];
         $product->save();
+
         return ProductResource::collection($product);
-    }
+        }
 
         /**
          * Remove the specified resource from storage.
@@ -109,7 +84,8 @@ class ProductController extends Controller
          */
         public function destroy(Product $product)
         {
-            $product->delete();
-            return response()->json(null, 204);
+         $product->delete();
+
+         return response()->json(null, 204);
         }
 }
